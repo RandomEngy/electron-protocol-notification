7082840db201f7fdc79a9688f4563726079627cb
David Rickard <davricka@microsoft.com>
2022-12-04 19:29:57 -0800

    Initial commit

---
+++ src/custom.d.ts
@@ -0,0 +1,3 @@
+// These do not have Typescript types so we need to just allow calling into them.
+declare module '@nodert-win10-rs4/windows.ui.notifications';
+declare module '@nodert-win10-rs4/windows.data.xml.dom';
